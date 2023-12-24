f8fe455fdd97ffb1c75f874f4d806b75d2db208b
Ayush800 <ayushpardhi800@gmail.com>
2023-12-24 15:12:03 +0530

    committed

---
+++ calculator.js
@@ -0,0 +1,28 @@
+let btnGroup = document.querySelectorAll(".buttons");
+let inputTag = document.querySelector("input");
+
+console.log(btnGroup);
+console.log(inputTag);
+
+let str = "";
+
+btnGroup.forEach (btn => {
+    btn.addEventListener("click" , e => {
+        if (e.target.innerHTML == "=") {
+            str = inputTag.value;
+            inputTag.value = eval(str);
+            console.log(str);
+        } else if (e.target.innerHTML == "AC") {
+            str = "";
+            inputTag.value = str;
+            console.log(str);
+        } else if (e.target.innerHTML == "DEL"){
+            str = str.substring(0, str.length-1);
+            inputTag.value = str;
+        } else {
+            str += e.target.innerHTML;
+            inputTag.value = str;
+            console.log(str);
+        }
+    });
+});
